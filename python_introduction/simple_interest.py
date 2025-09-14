ab567504ee29d4452cfa9cf6fe458da2f48ffd65
ghisia <munezeroghislaine50@gmail.com>
2025-09-14 22:54:09 +0200

    update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,4 +2,5 @@ principal = 1000
 rate = 0.05
 time = 3
 simple_interest = (principal * rate * time)
-print(f"The Simple Interest for principal {principal}, rate {rate}%, and time {time} years is: {simple_interest}")
+print(f"The Simple Interest for principal {principal}, rate {rate}%, and time {time} years")
+print("The simple interest is:", simple_interest)
