0108a6b76a2597a33edb43562b4348e8b4320cb6
ghisia <munezeroghislaine50@gmail.com>
2025-09-28 20:03:58 +0200

    Update

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,15 +9,15 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = int(input("Enter your choice: "))
 
-        if choice == '1':
+        if choice == 1:
             pass
-        elif choice == '2':
+        elif choice == 2:
             pass
-        elif choice == '3':
+        elif choice == 3:
             pass
-        elif choice == '4':
+        elif choice == 4:
             print("Goodbye!")
             break
         else:
